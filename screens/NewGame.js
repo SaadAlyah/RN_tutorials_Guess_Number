7cc9c85bb8f37c8abf3344c3df9b5a9ef0702a44
saadalyah <saadalyah@icloud.com>
2020-04-05 17:59:45 +0300

    design the New Game component

--- screens/NewGame.js
+++ screens/NewGame.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
 
-const Page_Name = props => {
+const NewGame = props => {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Start a New Game</Text>
+            <Text style={styles.title}>Start a New Game!</Text>
+            <View style={styles.card}>
+                <Text>Select a Number</Text>
+                <TextInput/>
+                <View style={styles.buttons}>
+                    <Button title="Reset" />
+                    <Button title="Confirm" />
+                </View>
+                
+            </View>
             
         </View>
-        
-        
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        
+        alignItems: 'center',
+        paddingTop: 20
     },
     title: {
-        fontSize: 32,
+        fontSize: 20,
+    },
+    card: {
+        justifyContent: "center",
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+        paddingTop: 20,
+    },
+    buttons: {
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        width: '100%',
+        margin: 25,
     },
 });
 
-export default Page_Name;
+export default NewGame;
